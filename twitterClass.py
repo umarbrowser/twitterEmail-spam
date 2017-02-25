1b942e89bb43392ec9888f912bf235a604eea18c
pyc0d3r <umarbrowser20@gmail.com>
2017-02-25 17:26:30 +0100

    update t

--- twitterClass.py
+++ twitterClass.py
@@ -1,20 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import urllib
 from anonBrowser import *
 import json
 import re
 import urllib2
+
+
 class reconPerson:
+
     def __init__(self, handle):
         self.handle = handle
         self.tweets = self.get_tweets()
+
     def get_tweets(self):
         query = urllib.quote_plus('from:' + self.handle+\
-                                  ' since:2009-01-01 include:retweets')
+          ' since:2009-01-01 include:retweets'
+                                  )
         tweets = []
         browser = anonBrowser()
         browser.anonymize()
-        response = browser.open('http://seach.twitter.com/'+\
-                                'search.json?q=' + query)
+        response = browser.open('http://search.twitter.com/'+\
+          'search.json?q=' + query)
+
         json_objects = json.load(response)
         for result in json_objects['results']:
             new_result = {}
@@ -23,14 +31,17 @@ class reconPerson:
             new_result['tweet'] = result['text']
             tweets.append(new_result)
         return tweets
-    def find_interrests(self):
+
+    def find_interests(self):
         interests = {}
         interests['links'] = []
         interests['users'] = []
         interests['hashtags'] = []
+
         for tweet in self.tweets:
             text = tweet['tweet']
-            links = re.compile('(http.*?)\Z|(http.*) ').findall(text)
+            links = re.compile('(http.*?)\Z|(http.*?) ').findall(text)
+
             for link in links:
                 if link[0]:
                     link = link[0]
@@ -38,6 +49,7 @@ class reconPerson:
                     link = link[1]
                 else:
                     continue
+
             try:
                 response = urllib2.urlopen(link)
                 full_link = response.url
@@ -45,29 +57,39 @@ class reconPerson:
             except:
                 pass
             interests['users'] +=\
-            re.compile('(@\w+)').findall(text)
+              re.compile('(@\w+)').findall(text)
             interests['hashtags'] +=\
-            re.compile('(#\w+)').findall(text)
-            interests['users'].sorts()
-            interests['hashtags'].sorts()
-            return interests
+              re.compile('(#\w+)').findall(text)
+
+        interests['users'].sort()
+        interests['hashtags'].sort()
+        interests['links'].sort()
+        return interests
+
     def twitter_locate(self, cityFile):
         cities = []
         if cityFile != None:
-            for line in open(cityFile).readline():
+            for line in open(cityFile).readlines():
                 city = line.strip('\n').strip('\r').lower()
                 cities.append(city)
-            locations = []
-            locCnt = 0
-            cityCnt = 0
-            tweetsText = ''
-            for tweet in self.tweets:
-                if tweet['geo'] != None:
-                    locations.append(tweet['geo'])
-                    locCnt += 1
-                tweetsText += tweet['tweet'].lower()
-            for city in cities:
-                if city in tweetsText:
-                    locations.append(city)
-                    cityCnt += 1
-            return locations
+
+        locations = []
+        locCnt = 0
+        cityCnt = 0
+        tweetsText = ''
+
+        for tweet in self.tweets:
+            if tweet['geo'] != None:
+                locations.append(tweet['geo'])
+                locCnt += 1
+
+            tweetsText += tweet['tweet'].lower()
+
+        for city in cities:
+            if city in tweetsText:
+                locations.append(city)
+                cityCnt += 1
+
+        return locations
+
+
